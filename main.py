958d34f48b1e0a9b332714f797f283219ca9b809
Fernanda Guimaraes <fehguimaraes90@gmail.com>
2025-07-16 12:52:26 -0300

    feature: Add code to use environment variables for Elasticsearch connection

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import requests
 from elasticsearch import Elasticsearch
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()  # Load environment variables from .env file
+
+elasticsearch_api_key = getenv("ELASTICSEARCH_API_KEY")
 
 # This function fetches METAR data for a given station and period from the Aviation Weather API.
 def fetch_metar(station, period):
@@ -9,18 +15,18 @@ def fetch_metar(station, period):
     return response.json()
 
 # This function indexes the fetched METAR data into an Elasticsearch index.
-def index_metar(metar_json, es_index, es_host, es_port, api_key='Wjl2RUU1Z0I5WnZHSDBxeEFHSy06THF6VC01MnJXbWdmUklFNmNFTFZDQQ=='):
+def index_metar(metar_json, es_index, es_host, es_port, api_key=elasticsearch_api_key):
     es = Elasticsearch([{'host': es_host, 'port': es_port, 'scheme': 'https'}], api_key=api_key)
     for record in metar_json:
         es.index(index=es_index, body=record)
 
 # Main function to fetch and index METAR data
 if __name__ == "__main__":
-    station = 'SBGR'  # Change to desired airport code
+    station = 'SBGL'  # Change to desired airport code
     period = 1
     es_index = 'skybuoy-metar-test-00001'
-    es_host = 'skybuoy-a7bc33.es.us-east-1.aws.elastic.cloud'  # Change to your Elasticsearch host
-    es_port = 443  # Change to your Elasticsearch port
+    es_host = getenv("ELASTICSEARCH_ENDPOINT")
+    es_port = 443
     metar_json = fetch_metar(station, period)
     index_metar(metar_json, es_index, es_host, es_port)
     print(metar_json)
